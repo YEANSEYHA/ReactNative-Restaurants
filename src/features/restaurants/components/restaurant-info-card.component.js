1dfd475f277eb0029d82f865773bd8f6659986bb
Yean Seyha <yeanseyha7@gmail.com>
2023-06-06 18:09:09 +0700

    Completed Single Card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/start";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: white;
@@ -9,46 +12,90 @@ const RestaurantCard = styled(Card)`
 
 const RestaurantCardCover = styled(Card.Cover)`
   padding: 20px;
-  background-color: ${(props) => props.theme.colors.ui.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  padding: 16px;
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom,: ${(props) => props.theme.space[2]};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg",
     photos = [
       "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg",
     ],
     address = "100 some randonm food",
     isOpenNow = true,
-    rating,
-    isClosedTemporarily,
+    rating = 4,
+    isClosedTemporarily = true,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
+
   return (
-    <RestaurantCard elevation={5} style={styles.card}>
+    <RestaurantCard elevation={5}>
       <RestaurantCardCover
         key={name}
-        style={styles.cover}
         source={{ uri: photos[0] }}
       ></RestaurantCardCover>
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20}></SvgXml>
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red", marginRight: 15 }}>
+                Closed Temporarily
+              </Text>
+            )}
+            {isOpenNow && <Open xml={open} width={20} height={20}></Open>}
+            <View style={{ paddingLeft: 16 }}>
+              <Image
+                source={{ uri: icon }}
+                style={{ width: 15, height: 15 }}
+              ></Image>
+            </View>
+          </SectionEnd>
+        </Section>
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "white",
-  },
-  cover: {
-    padding: 20,
-    backgroundColor: "white",
-  },
-});
